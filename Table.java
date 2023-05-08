a5db118a2b8a47651ac2e67fc1dca2811284546b
Megha Kushwah <125501270+meghakushwah@users.noreply.github.com>
2023-05-08 22:46:34 +0530

    Add files via upload

---
+++ Table.java
@@ -0,0 +1,17 @@
+public class Table {
+    public static void main(String[] args) {
+        int num = 5;
+        int i = 1;
+        int b;
+        
+        for (i = 1; i <= 10; i++) {
+        b = num * i;
+        System.out.println(b);
+        }
+        // do {
+        //     b = num * i;
+        //     System.out.println(b);
+        //     i++;
+        // } while (i <= 10);
+    }
+}
