47591eff7f23abfe15eba9a7ac35894bac4b25c6
Megha Kushwah <125501270+meghakushwah@users.noreply.github.com>
2023-05-08 22:43:04 +0530

    Add files via upload

---
+++ WhileLoop.java
@@ -0,0 +1,11 @@
+public class WhileLoop {
+    public static void main(String[] args) {
+        int i = 100;
+        while (i >= 1) {
+            System.out.println(i);
+            i--;
+        }
+    }
+}
+
+
