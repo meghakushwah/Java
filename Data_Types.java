f79c0c5cc74ffd1be6d79d06c15519070c5ea985
Megha Kushwah <125501270+meghakushwah@users.noreply.github.com>
2023-05-08 22:37:37 +0530

    Add files via upload

---
+++ Data_Types.java
@@ -0,0 +1,17 @@
+public class Data_Types {
+
+    public static void main(String[] args) {
+
+        System.out.println("Double Min:" + Double.MIN_VALUE);
+        System.out.println("Double Max:" + Double.MAX_VALUE);
+        System.out.println("Double Bytes:" + Double.BYTES);
+
+        System.out.println();
+
+        System.out.println("Int Min:" + Integer.MIN_VALUE);
+        System.out.println("Int Max:" + Integer.MAX_VALUE);
+        System.out.println("Int Bytes:" + Integer.BYTES);
+    }
+}
+
+
