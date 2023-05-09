14343272da5d5c9433df81b28f18376f199e9106
Megha Kushwah <125501270+meghakushwah@users.noreply.github.com>
2023-05-09 23:21:12 +0530

    Add files via upload

---
+++ Array_2D.java
@@ -0,0 +1,17 @@
+ public class Array_2D {
+
+	public static void main(String[] args) {
+		
+            int A[][]={ {1,2,3}, {4,5,6}, {7,8,9} };
+	        
+	        for(int i=0; i<A.length; i++){
+	            for(int j=0; j<A[0].length; j++){
+	                System.out.print(A[i][j]+" ");
+	            }
+	             System.out.println(" ");
+	        }
+	        
+	}
+
+}
+ 
